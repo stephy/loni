91ad3cc8370a145a0e70387588a69f9ce9c3e298
Stephy <alves@stephanimoroni.com>
2011-10-30 18:33:57 -0700

    fixed add_tabs js

--- javascripts/add_tabs.js
+++ javascripts/add_tabs.js
@@ -16,24 +16,19 @@ jQuery(document).ready(function(){
 	//create new canvas on create_blank (+ tab button) click
 	$('#add_new_canvas').click(function(){
 		var new_canvas_index = nextCanvasIndex();
-		$('<li><img src="images/icon-area.gif" class="button-close" id="close-canvas-'+ new_canvas_index +'" />Untitled</li>').insertBefore('#add_new_canvas');
-		$('<div id="canvas-'+ new_canvas_index +'" class="canvas" width="300" height="300"></div>').appendTo('.canvae');
+		var new_canvas_id = 'canvas-'+ new_canvas_index;
+		$('.tabs li:eq(0)').clone().insertBefore('#add_new_canvas');
+		$('<div id="'+ new_canvas_id +'" class="canvas"></div>').appendTo('.canvae');
 		//display the new/recently created canvas only, hide all others
-		for(i=0; i<$('.canvas').length; i++){
-			if(i== new_canvas_index){
-				$('#canvas-'+ i).css('display', 'block');
-			}
-			else{
-				$('#canvas-'+ i).css('display', 'none');
-			}	
-		}
+		$('.canvas').hide();
+		$('#'+new_canvas_id).show();
 	});
 	
 	
 	//close canvas on close ( x tab button) click
-	$('#close-canvas-1').click(function(){
-		$('#close-canvas-1').closest('li').remove();
-		$('#canvas-1').remove();
+	$('.button-close').click(function(){
+		$(this).parent().remove()
+		//$('#canvas-1').remove();
 	});
 	
 });
